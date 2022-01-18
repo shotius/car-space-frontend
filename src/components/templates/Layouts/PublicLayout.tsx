1a07f98c78f727d1eccb0d42d4906aed194cc239
shotius <sh.archemashvili@gmail.com>
2022-01-18 14:51:50 +0400

    refactor and clean up

--- src/components/templates/Layouts/PublicLayout.tsx
+++ src/components/templates/Layouts/PublicLayout.tsx
@@ -32,7 +32,7 @@ export const PublicLayout: React.FC<PublicLayoutProps> = ({ children }) => {
       <Container
         maxW="1920px"
         bg="#F0F0F0"
-        minH="190vh"
+        minH="90vh"
         pb={['64px', '85px', '101px', '138px']}
         pl="0"
         pr="0"
