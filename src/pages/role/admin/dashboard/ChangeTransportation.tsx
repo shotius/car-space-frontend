4b9dd518c10f2d2e3929d729ac406e3120257b61
shotius <sh.archemashvili@gmail.com>
2022-05-29 17:23:07 +0400

    Search with debounce

--- src/pages/role/admin/dashboard/ChangeTransportation.tsx
+++ src/pages/role/admin/dashboard/ChangeTransportation.tsx
@@ -2,12 +2,15 @@ import {
   Center,
   CloseButton,
   HStack,
+  Input,
   InputGroup,
   InputProps,
   InputRightElement,
   VStack,
 } from '@chakra-ui/react';
-import React, { useEffect, useState } from 'react';
+import { setServers } from 'dns/promises';
+import { debounce } from 'lodash';
+import React, { useEffect, useMemo, useState } from 'react';
 import { ContainerOuter } from 'src/components/atoms/Containers/ContainerOuter';
 import { SearchIcon } from 'src/components/atoms/Icons/SearchIcon';
 import { EditButton } from 'src/components/molecules/Buttons/EditButton';
@@ -19,85 +22,87 @@ import { ITransportDataObject } from '../../../../../../server/shared_with_front
 
 interface ChangeTransportationProps {}
 
-interface InputInterface {
-  onSubmit?: any;
-}
-
-const SearchComponent: React.FC<InputProps & InputInterface> = ({
-  onSubmit = (e: React.SyntheticEvent) => {},
-  ...rest
-}) => {
-  const [searchWord, setSearchWord] = useState('');
-
-  return (
-    <InputGroup>
-      <InputGrey
-        value={searchWord}
-        onChange={(e) => setSearchWord(e.currentTarget.value)}
-        placeholder="Search"
-        {...rest}
-      />
-      <InputRightElement
-        children={<SearchIcon color="gray.300" />}
-        cursor="pointer"
-        onClick={onSubmit}
-      />
-    </InputGroup>
-  );
-};
-
 const ChangeTransportation: React.FC<ChangeTransportationProps> = ({}) => {
   const [transportationData, setTransportationData] = useState<
     ITransportDataObject[]
   >([]);
 
+  const [searchWord, setSearchWord] = useState('');
+  const [isFetching, setIsFetching] = useState(true);
+
+  function changeHandler(e: React.SyntheticEvent<HTMLInputElement>) {
+    setSearchWord((e.target as HTMLInputElement).value);
+  }
+
+  const debouncedChangeHandler = useMemo(
+    () => debounce(changeHandler, 600),
+    []
+  );
+
   useEffect(() => {
     transportaionService
       .getTransportationData()
-      .then(({ results }) => setTransportationData(results));
+      .then(({ results }) => setTransportationData(results))
+      .finally(() => setIsFetching(false));
   }, []);
+
+  const dataToShow = useMemo(() => {
+    if (searchWord && transportationData.length) {
+      const filteredResult = transportationData.filter(
+        (data) =>
+          data.auction.includes(searchWord) || data.city.includes(searchWord)
+      );
+      return filteredResult;
+    } else {
+      return transportationData;
+    }
+  }, [searchWord, transportationData]);
 
   return (
     <ContainerOuter>
       <Center>
         <Card>
-          <SearchComponent onSubmit={() => console.log('clicked and filter')}/>
+          <InputGrey onChange={debouncedChangeHandler} placeholder="Search" />
           <VStack w="full" spacing="0">
-            {transportationData.map((data) => (
-              <VStack
-                cursor="pointer"
-                p="4"
-                borderBottom={'1px solid #00000029'}
-                className="hoverable"
-                w="full"
-                align={'start'}
-                position="relative"
-              >
-                <HStack pos="absolute" right="14px">
-                  <EditButton boxSize={6} />
-                  <CloseButton />
-                </HStack>
-                <TextRegular>
-                  <b>Auction:</b> {data.auction}
-                </TextRegular>
+            {isFetching ? (
+              <p>...fetching</p>
+            ) : (
+              dataToShow.map((data) => (
+                <VStack
+                  cursor="pointer"
+                  p="4"
+                  borderBottom={'1px solid #00000029'}
+                  className="hoverable"
+                  w="full"
+                  align={'start'}
+                  position="relative"
+                >
+                  <HStack pos="absolute" right="14px">
+                    <EditButton boxSize={6} />
+                    <CloseButton />
+                  </HStack>
+                  <TextRegular>
+                    <b>Auction:</b> {data.auction}
+                  </TextRegular>
 
-                <TextRegular>
-                  <b>Citi:</b> {data.city}
-                </TextRegular>
+                  <TextRegular>
+                    <b>Citi:</b> {data.city}
+                  </TextRegular>
 
-                <TextRegular>
-                  <b>State:</b> {data.state}
-                </TextRegular>
+                  <TextRegular>
+                    <b>State:</b> {data.state}
+                  </TextRegular>
 
-                <TextRegular>
-                  <b>Price:</b> {data.price}
-                </TextRegular>
+                  <TextRegular>
+                    <b>Price:</b> {data.price}
+                  </TextRegular>
 
-                <TextRegular>
-                  <b>Zip:</b> {data.zip}
-                </TextRegular>
-              </VStack>
-            ))}
+                  <TextRegular>
+                    <b>Zip:</b> {data.zip}
+                  </TextRegular>
+                </VStack>
+              ))
+            )}
           </VStack>
         </Card>
       </Center>
