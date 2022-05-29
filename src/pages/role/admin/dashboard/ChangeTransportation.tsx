620bf90f9e153a6f3fce864f68020ea66ec70ddc
shotius <sh.archemashvili@gmail.com>
2022-05-30 00:34:10 +0400

    Add Select

--- src/pages/role/admin/dashboard/ChangeTransportation.tsx
+++ src/pages/role/admin/dashboard/ChangeTransportation.tsx
@@ -1,18 +1,7 @@
-import {
-  Center,
-  CloseButton,
-  HStack,
-  Input,
-  InputGroup,
-  InputProps,
-  InputRightElement,
-  VStack,
-} from '@chakra-ui/react';
-import { setServers } from 'dns/promises';
+import { Center, CloseButton, HStack, VStack } from '@chakra-ui/react';
 import { debounce } from 'lodash';
 import React, { useEffect, useMemo, useState } from 'react';
 import { ContainerOuter } from 'src/components/atoms/Containers/ContainerOuter';
-import { SearchIcon } from 'src/components/atoms/Icons/SearchIcon';
 import { EditButton } from 'src/components/molecules/Buttons/EditButton';
 import { Card } from 'src/components/molecules/Cards/Card';
 import { InputGrey } from 'src/components/molecules/Inputs/InputGrey';
@@ -27,6 +16,9 @@ const ChangeTransportation: React.FC<ChangeTransportationProps> = ({}) => {
     ITransportDataObject[]
   >([]);
 
+  const [page, setPage] = useState(0);
+  const [perPage, setPerPage] = useState(10);
+
   const [searchWord, setSearchWord] = useState('');
   const [isFetching, setIsFetching] = useState(true);
 
@@ -34,8 +26,17 @@ const ChangeTransportation: React.FC<ChangeTransportationProps> = ({}) => {
     setSearchWord((e.target as HTMLInputElement).value);
   }
 
+  function handlePerPageSelect(e: React.SyntheticEvent<HTMLSelectElement>) {
+    setPerPage(+e.currentTarget.value);
+  }
+
   const debouncedChangeHandler = useMemo(
-    () => debounce(changeHandler, 600),
+    () => debounce(changeHandler, 300),
+    []
+  );
+
+  const debouncePerPageSelect = useMemo(
+    () => debounce(handlePerPageSelect, 300),
     []
   );
 
@@ -50,7 +51,8 @@ const ChangeTransportation: React.FC<ChangeTransportationProps> = ({}) => {
     if (searchWord && transportationData.length) {
       const filteredResult = transportationData.filter(
         (data) =>
-          data.auction.includes(searchWord) || data.city.includes(searchWord)
+          data.auction.toLowerCase().includes(searchWord.toLowerCase()) ||
+          data.city.toLowerCase().includes(searchWord.toLowerCase())
       );
       return filteredResult;
     } else {
@@ -62,46 +64,58 @@ const ChangeTransportation: React.FC<ChangeTransportationProps> = ({}) => {
     <ContainerOuter>
       <Center>
         <Card>
+          <HStack>
+            <select onChange={debouncePerPageSelect}>
+              {[10, 20, 30, 40, 50].map((pageSize) => (
+                <option key={pageSize} value={pageSize}>
+                  Show: {pageSize}
+                </option>
+              ))}
+            </select>
+            <TextRegular>Page: 1</TextRegular>
+          </HStack>
           <InputGrey onChange={debouncedChangeHandler} placeholder="Search" />
           <VStack w="full" spacing="0">
             {isFetching ? (
               <p>...fetching</p>
             ) : (
-              dataToShow.map((data) => (
-                <VStack
-                  cursor="pointer"
-                  p="4"
-                  borderBottom={'1px solid #00000029'}
-                  className="hoverable"
-                  w="full"
-                  align={'start'}
-                  position="relative"
-                >
-                  <HStack pos="absolute" right="14px">
-                    <EditButton boxSize={6} />
-                    <CloseButton />
-                  </HStack>
-                  <TextRegular>
-                    <b>Auction:</b> {data.auction}
-                  </TextRegular>
+              <>
+                {dataToShow.slice(0, 10).map((data) => (
+                  <VStack
+                    cursor="pointer"
+                    p="4"
+                    borderBottom={'1px solid #00000029'}
+                    className="hoverable"
+                    w="full"
+                    align={'start'}
+                    position="relative"
+                  >
+                    <HStack pos="absolute" right="14px">
+                      <EditButton boxSize={6} />
+                      <CloseButton />
+                    </HStack>
+                    <TextRegular>
+                      <b>Auction:</b> {data.auction}
+                    </TextRegular>
 
-                  <TextRegular>
-                    <b>Citi:</b> {data.city}
-                  </TextRegular>
+                    <TextRegular>
+                      <b>Citi:</b> {data.city}
+                    </TextRegular>
 
-                  <TextRegular>
-                    <b>State:</b> {data.state}
-                  </TextRegular>
+                    <TextRegular>
+                      <b>State:</b> {data.state}
+                    </TextRegular>
 
-                  <TextRegular>
-                    <b>Price:</b> {data.price}
-                  </TextRegular>
+                    <TextRegular>
+                      <b>Price:</b> {data.price}
+                    </TextRegular>
 
-                  <TextRegular>
-                    <b>Zip:</b> {data.zip}
-                  </TextRegular>
-                </VStack>
-              ))
+                    <TextRegular>
+                      <b>Zip:</b> {data.zip}
+                    </TextRegular>
+                  </VStack>
+                ))}
+              </>
             )}
           </VStack>
         </Card>
